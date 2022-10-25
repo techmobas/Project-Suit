dd1ab6865e1b02cc540bf5df6574337b80336113
exian1992 <durayphilip1992@gmail.com>
2022-10-25 17:25:34 +0700

    - Create further mechanic for the RPS System - Modify SampleScene to test the RPS Mechanic

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,16 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public RPSControl rpsControl;
+
+    private RPSInput rpsInput;
+  
+    private void Awake()
     {
-        
+        rpsInput = new RPSInput();
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        rpsInput.Enable();
+
+        rpsInput.Player.Rock.started += Rock;
+        rpsInput.Player.Rock.performed += Rock;
+        rpsInput.Player.Rock.canceled += Rock;
+
+        rpsInput.Player.Paper.started += Paper;
+        rpsInput.Player.Paper.performed += Paper;
+        rpsInput.Player.Paper.canceled += Paper;
+
+        rpsInput.Player.Scissors.started += Scissors;
+        rpsInput.Player.Scissors.performed += Scissors;
+        rpsInput.Player.Scissors.canceled += Scissors;
+    }
+
+    private void OnDisable()
+    {
+        rpsInput.Disable();
+
+        rpsInput.Player.Rock.started -= Rock;
+        rpsInput.Player.Paper.started -= Paper;
+        rpsInput.Player.Scissors.started -= Scissors;
+    }
+
+    private void Start()
+    {
+        ;
+    }
+
+    private void Rock(InputAction.CallbackContext context)
+    {
+        Debug.Log("I choose Rock");
+        context.ReadValue<float>();
+        context.ReadValueAsButton();
+    }
+
+    private void Paper(InputAction.CallbackContext context)
+    {
+        Debug.Log("I choose Peper");
+        context.ReadValue<float>();
+        context.ReadValueAsButton();
+    }
+
+    private void Scissors(InputAction.CallbackContext context)
+    {
+        Debug.Log("I choose Scissors");
+        context.ReadValue<float>();
+        context.ReadValueAsButton();
+    }
+
     void Update()
     {
         
--- Assets/Script/RPSControl.cs
+++ Assets/Script/RPSControl.cs
@@ -1,70 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RPSControl : MonoBehaviour
 {
     public enum Choices {
         ROCK = 1, 
-        PAPER = 2, 
-        SCISSORS = 3}
+        PAPER, 
+        SCISSORS}
 
     private int playerInt = -1;
     private int botInt = -1;
 
     private bool playerTurn = true;
-    
-    void Update(){
+
+    [SerializeField] private TextMeshProUGUI playerText;
+    [SerializeField] private TextMeshProUGUI botText;
+    [SerializeField] private TextMeshProUGUI resultText;
+
+    private void Start()
+    {
+    }
+
+    private void Update(){
+        ChangeText();
+
         if (playerTurn && playerInt == -1) return;
 
         else{
             BotChoose();
+            CheckWinner();
+            playerInt = -1;
+            playerTurn = true;
+        }
+    }
+
+    private void ChangeText()
+    {
+        if (playerInt == (int)Choices.ROCK)
+        {
+            playerText.text = "ROCK";
+        }
+
+        else if (playerInt == (int)Choices.PAPER)
+        {
+            playerText.text = "PAPER";
+        }
+
+        else if (playerInt == (int)Choices.SCISSORS)
+        {
+            playerText.text = "SCISSORS";
         }
     }
 
     private void CheckWinner(){
         if(playerInt == botInt){
-            //DRAW
+            //D
+            resultText.text = "DRAW";
         }
 
         else if (playerInt == (int)Choices.ROCK && botInt == (int)Choices.PAPER){
-            //LOSE
+            resultText.text = "LOSE";
         }
 
         else if (playerInt == (int)Choices.ROCK && botInt == (int)Choices.SCISSORS)
         {
             //WIN
+            resultText.text = "WIN";
         }
         else if (playerInt == (int)Choices.PAPER && botInt == (int)Choices.SCISSORS)
         {
             //LOSE
+            resultText.text = "LOSE";
         }
 
         else if (playerInt == (int)Choices.PAPER && botInt == (int)Choices.ROCK)
         {
             //WIN
+            resultText.text = "WIN";
         }
         else if (playerInt == (int)Choices.SCISSORS && botInt == (int)Choices.ROCK)
         {
             //LOSE
+            resultText.text = "LOSE";
         }
 
         else if (playerInt == (int)Choices.SCISSORS && botInt == (int)Choices.PAPER)
         {
             //WIN
+            resultText.text = "WIN";
         }
     }
 
-    public int PlayerInt(){
-        return playerInt;
-    }
-
-    public void PlayerChoose(){
-        PlayerInt();
+    public void PlayerChoose(int choose){
+        playerInt = choose;
         playerTurn = false;
     }
 
     public void BotChoose(){
         botInt = Random.Range(1, 3);
+
+        if (botInt == (int)Choices.ROCK)
+        {
+            botText.text = "ROCK";
+        }
+
+        else if (botInt == (int)Choices.PAPER)
+        {
+            botText.text = "PAPER";
+        }
+
+        else if (botInt == (int)Choices.SCISSORS)
+        {
+            botText.text = "SCISSORS";
+        }
     }
 }
